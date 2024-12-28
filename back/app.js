eb4b3c44f4de187e14e80fb0cfe5d486de748dcd
AnamariaGreceanu <greceanu.anamaria1@gmail.com>
2024-12-28 10:49:02 +0200

    initialized express project

---
+++ back/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cors = require("cors");
+const router = require('./routes/index');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use('/api', router);
+
+app.listen(process.env.PORT || 8000, ()=> {
+    console.log("Server started listening on port " + process.env.PORT);
+})
---
+++ back/middlewares/auth.js
@@ -0,0 +1,6 @@
+const jwt = require("jsonwebtoken");
+const secret = process.env.SECRET_TOKEN;
+
+const authenticateToken = async (req, res, next) => { }
+
+module.exports=authenticateToken
